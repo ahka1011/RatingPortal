9f725afe9dff793349d35b1b39aa12acd2a9ddf6
Tülin Duman <dutu1011@hs-karlsruhe.de>
2017-06-23 15:47:42 +0200

    Klasse Gruppe umbenannt zu Group und übersetzt in das Englische

---
+++ src/main/java/com/rating/domain/Group.java
@@ -0,0 +1,48 @@
+package com.rating.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity(name="group")
+public class Group {
+	@Id
+	private int group_nr;
+	@Column
+	private String description;
+	@Column
+	private int course_nr;
+	
+	public Group() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Group(int group_nr, String description, int course_nr) {
+		super();
+		this.group_nr = group_nr;
+		this.description = description;
+		this.course_nr = course_nr;
+	}
+	public int getGroup_nr() {
+		return group_nr;
+	}
+	public void setGroup_nr(int group_nr) {
+		this.group_nr = group_nr;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getCourse_nr() {
+		return course_nr;
+	}
+	public void setCourse_nr(int course_nr) {
+		this.course_nr = course_nr;
+	}
+	@Override
+	public String toString() {
+		return "Group [groupn_nr=" + group_nr + ", description=" + description + ", course_nr=" + course_nr + "]";
+	}
+}
