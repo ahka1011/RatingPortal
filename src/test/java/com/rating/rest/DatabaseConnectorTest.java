f94aba07853f322a26b2486304ac65a6f4ea8483
tolgaduman <tolgakan@DESKTOP-G7728CQ>
2017-06-24 01:38:03 +0200

    Assert.assertNotNull in die Testmethoden eingebaut

--- src/test/java/com/rating/rest/DatabaseConnectorTest.java
+++ src/test/java/com/rating/rest/DatabaseConnectorTest.java
@@ -4,20 +4,22 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import junit.framework.Assert;
+
 public class DatabaseConnectorTest {
 
 	@Test
 	public void studentKursListeSuchenStudentDa() {
 		final String iz_kuerzel_working = "ahka1011";
 		assertEquals("Student sollte da sein", 1, DatabaseConnector.studentKursListeSuchen(iz_kuerzel_working).size());
-	
+		Assert.assertNotNull(iz_kuerzel_working);
 	}
 
-	
 	@Test
 	public void studentKursListeSuchenStudentNichtDa() {
 		final String iz_kuerzel_not_working = "asadfhakdsf";
 		assertEquals("Student sollte nicht da sein", 0, DatabaseConnector.studentKursListeSuchen(iz_kuerzel_not_working).size());
+		Assert.assertNotNull(iz_kuerzel_not_working);
 	}
 	
 	
@@ -25,13 +27,18 @@ public class DatabaseConnectorTest {
 	public void dozentKursListeSuchenDa() {
 		final String iz_kuerzel_working = "stin0001";
 		assertEquals("Student sollte da sein", 6, DatabaseConnector.dozentKursListeSuchen(iz_kuerzel_working).size());
-	
+		Assert.assertNotNull(iz_kuerzel_working);
 	}
-
+	
 	
 	@Test
 	public void dozentKursListeSuchenNichtDa() {
 		final String iz_kuerzel_not_working = "asadfhakdsf";
 		assertEquals("Student sollte nicht da sein", 0, DatabaseConnector.dozentKursListeSuchen(iz_kuerzel_not_working).size());
+		Assert.assertNotNull(iz_kuerzel_not_working);
 	}
+	
+	
+	
+	
 }
